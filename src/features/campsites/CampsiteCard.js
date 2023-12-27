fce3fb443a5e248fb856f0c4074e37dd2d3aaa2d
edmund80 <edmund.e.davis@gmail.com>
2023-12-27 09:32:28 -0500

    added CampsiteDetail and refactored code

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-const CampsiteCard = (props) => {
-    console.log('Campsite:', props.campsite);
+const CampsiteCard = ({ campsite }) => {
+    const { image, name } = campsite
     
     return (
         <Card>
             <CardImg
                 width='100%'
-                src={props.campsite.image}
-                alt={props.campsite.name}
+                src={image}
+                alt={name}
             />
             <CardImgOverlay>
-                <CardTitle>{props.campsite.name}</CardTitle>
+                <CardTitle>{name}</CardTitle>
             </CardImgOverlay>
         </Card>
     );
