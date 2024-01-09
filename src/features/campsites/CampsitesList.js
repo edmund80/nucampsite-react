9f4925192b404d91b0b61737ad74e58746c63547
edmund80 <edmund.e.davis@gmail.com>
2024-01-09 12:41:35 -0500

    Updated Comment From Validation

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -10,12 +10,11 @@ const CampsitesList = () => {
         <Row className='ms-auto'>
             {campsites.map((campsite) => {
                 return (
-                    <Col md='5' 
-                         className='m-4' 
-                         key={campsite.id}
+                    <Col md='5'
+                        className='m-4'
+                        key={campsite.id}
                     >
-                       
-                       <CampsiteCard campsite={campsite} />
+                        <CampsiteCard campsite={campsite} />
                     </Col>
                 );
             })}
